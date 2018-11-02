374d4c45133fa8489ae1e8eb385dc74a507049f4
NestY1973 <ynester@yandex.ru>
2018-11-02 23:21:50 +0300

    ВЫполнил задания части easy

--- lesson_05/home_work/hw05_easy.py
+++ lesson_05/home_work/hw05_easy.py
@@ -3,12 +3,38 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+#Создание списка директорий
+try:
+    for el in range(9):
+        dir_path = os.path.join(os.getcwd(),'dir_'+str(el+1))
+        os.mkdir(dir_path)
+except FileExistsError:
+    print('Такая директория уже существует')
+
+#Удаление списка директорий
+try:
+    for el in range(9):
+        dir_path = os.path.join(os.getcwd(),'dir_'+str(el+1))
+        os.rmdir(dir_path)
+except FileExistsError:
+    print('Такая директория не существует')
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+dir_path = os.path.join(os.getcwd())
+dir_list=os.listdir(dir_path)
+for d in dir_list:
+    dir_name=os.path.join(dir_path,d)
+    if os.path.isdir(dir_name):
+        print(d)
 
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-
+import shutil
+file_name=os.path.basename(__file__)
+newfile=file_name+'.dupl'
+shutil.copy(file_name,newfile)
